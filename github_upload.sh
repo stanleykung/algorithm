ff942a4b63ff4a2215d41a5edd59ef9a3281dca5
stanleykung <kungdehan@gmail.com>
2018-11-12 15:51:03 +0800

    upload_lazy_bash_script

--- github_upload.sh
+++ github_upload.sh
@@ -1,6 +1,6 @@
 git add .
 read -p "Enter commit message: " commit
 git commit -m $commit
-git git push origin master
+git push origin master
 
 #https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line/
