9e69d5febff558b567634d710efc9b029e4e14db
stanleykung <kungdehan@gmail.com>
2019-01-27 01:34:40 -0800

    update the selection sort code for less swap time

--- SelectSort/SelectSort.c
+++ SelectSort/SelectSort.c
@@ -21,11 +21,21 @@ int main(){
 	int arr[SIZE]={5,3,8,6,2,7,1,4};
 	print_arr(arr);
 
-	for(int i=0; i<SIZE; i++){
+	/*for(int i=0; i<SIZE; i++){
 		for(int j=i+1; j<SIZE; j++){
 			if(arr[j] < arr[i])
 				swap(&arr[i], &arr[j]);
 		}
+	}*/
+	for(int i=0; i<SIZE-1; i++){
+		int min_index = i;
+		for(int j=i+1; j<SIZE; j++){
+			if(arr[j] < arr[min_index])
+				min_index = j;
+		}
+		if(min_index!=i)
+			swap(&arr[i], &arr[min_index]);
 	}
+
 	print_arr(arr);
 }
