50a7284d104fee0edca6796a51a88accbc77a8db
stanleykung <kungdehan@gmail.com>
2019-01-30 11:00:39 +0800

    upload the BST code with delete function

--- BinarySearchTree/BST.c
+++ BinarySearchTree/BST.c
@@ -3,6 +3,7 @@
 
 struct BST{
     int val;
+    //struct BST * parent;
     struct BST * leftchild;
     struct BST * rightchild;
 };
@@ -47,15 +48,78 @@ BST * search_BST(BST * node, int key){
     return NULL;
 }
 
+BST * findMin_BST(BST * node){
+    BST * current = node;
+    while(current->leftchild)
+        current = current->leftchild;
+    return current;
+}
+
+BST * delete_BST(BST * node, int key){
+    //BST * deleted_node = search_BST(node, key);
+    if(node==NULL) return node;
+
+    if(node->val > key)
+        node->leftchild = delete_BST(node->leftchild,key);
+    else if(node->val < key)
+        node->rightchild = delete_BST(node->rightchild,key);
+    //case for node->val == key (the node exact what we need to delete it)
+    else{
+        //no child -> directly delete the node
+        if(node->leftchild == NULL && node->rightchild == NULL){
+            free(node);
+            return NULL;
+        }
+
+        // only one child and is right child which should be returned to higher level recursive
+        else if(node->leftchild == NULL){
+            BST * temp = node->rightchild;
+            free(node);
+            return temp;
+        }
+
+        //only one child and is left child
+        else if(node->rightchild == NULL){
+            BST * temp = node->leftchild;
+            free(node);
+            return temp;
+        }
+
+        //both two children are existed. -> use the minimum of right subtree to replace the node
+        else {
+            BST * temp = findMin_BST(node->rightchild);
+            node->val = temp->val;
+            delete_BST(node->rightchild,temp->val);
+            return node;
+        }
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     BST * root = NULL;
-    root = insert_BST(root,30);
-    root = insert_BST(root,40);
+    root = insert_BST(root,15);
     root = insert_BST(root,5);
-    root = insert_BST(root,2);
+    root = insert_BST(root,3);
+    root = insert_BST(root,12);
+    root = insert_BST(root,10);
+    root = insert_BST(root,13);
+    root = insert_BST(root,6);
+    root = insert_BST(root,7);
+    root = insert_BST(root,16);
+    root = insert_BST(root,20);
+    root = insert_BST(root,18);
+    root = insert_BST(root,23);
     tree_traveral_preorder(root);
-    BST * find = search_BST(root,5);
-    printf("%d",find->val);
+    //BST * find = search_BST(root,5);
+    //printf("%d",find->val);
+    printf("\n");
+    root = delete_BST(root,5);
+    tree_traveral_preorder(root);
+    printf("\n");
     return 0;
 }
+
+
+//https://www.geeksforgeeks.org/deletion-binary-tree/
+//https://www.geeksforgeeks.org/bfs-vs-dfs-binary-tree/
